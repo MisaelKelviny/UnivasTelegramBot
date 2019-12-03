9c4bf998acd530747308d0a650d59f0bebb7f239
MisaelKelviny <misaelmbass@gmail.com>
2019-12-03 20:35:52 -0300

    subindo as ultimas alterações

--- core.py
+++ core.py
@@ -4,6 +4,9 @@ from functools import wraps
 import apiai
 import json
 import re
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
 # Settings
 # Telegram API Token
@@ -12,29 +15,24 @@ dispatcher = updater.dispatcher
 
 def send_typing_action(func):
     """Sends typing action while processing func command."""
-
     @wraps(func)
     def command_func(update, context, *args, **kwargs):
         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
         return func(update, context,  *args, **kwargs)
-
     return command_func
 
 @send_typing_action
 def my_handler(update, context):
     pass # Will send 'typing' action while processing the request.
 
-
 # Command processing
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text='Olá, Bem vindo ao chat da UNIVAS! Em que posso ajudar ?')
 
-
 def boleto(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text='Seu boleto pode ser encontrado dentro do portal do aluno \n\n Acesse: http://portalaluno.univas.edu.br/SistemaWeb/mentornet/centralaluno/PA_Login_1.php \n\n')
-
 
 def contato(bot, update):
     msg = "0800 039 0010 (Ligação Gratuita)\n\n\n"
@@ -43,16 +41,17 @@ def contato(bot, update):
     msg += "Unidade Fátima: \n Avenida Pref. Tuany Toledo, 470 - CEP 37554-210 - Pouso Alegre, MG \n Telefone: (35) 3449 9257 \n E-mail: fafiep@univas.edu.br\n\n"
     bot.send_message(chat_id=update.message.chat_id, text=msg)
 
-
 def incentivo(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text="Temos um texto especial sobre o incentivo do PROGRAMA DE INCENTIVO À PESQUISA: \n\n Veja mais em: https://www.univas.edu.br/docs/2018/pesquisa/incentivo.pdf \n\n")
 
+def portalAluno(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="O portal do Aluno pode ser acesso atráves do link: \n http://portalaluno.univas.edu.br/SistemaWeb/mentornet/centralaluno/PA_Login_1.php")
 
 def nucleo(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text="Temos um texto especial sobre NÚCLEOS DE PESQUISAS: \n\n Veja mais em: https://www.univas.edu.br/docs/2018/pesquisa/nucleoPesquisa.pdf \n\n")
-
 
 def convenio(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
@@ -63,13 +62,17 @@ def grupoPesquisa(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text="Saiba mais sobre os grupos de pesquisa \n\n Veja mais em: https://www.univas.edu.br/docs/2019/pesquisa/gruposDePesquisa/Grupos%20de%20Pesquisa%20CNPq.pdf \n\n")
 
+def editais(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Os editais são disponibilizados dentro do site da univas, você pode visualizar em: https://www.univas.edu.br/maisEditais.asp")
+
+def filtro(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Olha o Zé!")
 
 def pesquisa(bot, update):
     msg = "A Pesquisa Científica é uma das três áreas onde a Universidade deve atuar. A busca pelo conhecimento, de forma sistemática e contínua, difere uma verdadeira Universidade de outras instituições de ensino superior, impactando de maneira positiva o seu Ensino e as suas atividades de Extensão, aumentando assim sua relevância no contexto local, regional, nacional e internacional. \n\n \
     A Universidade do Vale do Sapucaí (Univás), ciente de sua participação e incentivo a pesquisa científica, coloca como meta principal a busca constante pelo conhecimento. Realizada por docentes qualificados e por seus acadêmicos, a Pesquisa Científica na Univás tem o seu gerenciamento centrado na Coordenadoria de Pesquisa, subordinada à Pró-Reitoria de Pós-Graduação e Pesquisa da Univás. \
     A Coordenadoria tem como missão estimular docentes e acadêmicos a se engajarem em projetos científicos cada vez com melhor qualidade e em maior número."
     bot.send_message(chat_id=update.message.chat_id, text=msg)
-
 
 def etica(bot, update):
     msg = "Comissão de Ética \n\
@@ -86,7 +89,6 @@ def etica(bot, update):
     ceua@univas.edu.br \n\n"
     bot.send_message(chat_id=update.message.chat_id, text=msg)
 
-
 def secretaria(bot, update):
     msg = "Endereço: \n\
     Avenida Prefeito Tuany Toledo, 470, Fátima I \
@@ -100,25 +102,18 @@ def secretaria(bot, update):
 def vestibular(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Fique por dentro das datas dos nossos vestibulates em: \n\n http://portalvestibular.univas.edu.br/portalVestibular2020/index.asp?utm_source=site&utm_medium=botao&utm_campaign=vestibular2020")
 
-
-# essa tem que ser sempre a ultima
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id,
-                             text="Desculpe não entendi o que você quiz dizer")
-
-
 # Handlers
-start_command_handler = MessageHandler((Filters.regex(re.compile(r'oi', re.IGNORECASE)) | \
+start_command_handler = MessageHandler((Filters.regex(re.compile(r'eae', re.IGNORECASE)) | \
     (Filters.regex(re.compile(r'oi', re.IGNORECASE))) | \
     (Filters.regex(re.compile(r'ola', re.IGNORECASE))) | \
     (Filters.regex(re.compile(r'olá', re.IGNORECASE))) | \
     (Filters.regex(re.compile(r'opa', re.IGNORECASE))) | \
     (Filters.regex(re.compile(r'bom dia', re.IGNORECASE))) | \
     (Filters.regex(re.compile(r'boa tarde', re.IGNORECASE))) | \
-    (Filters.regex(re.compile(r'boa noite', re.IGNORECASE)))), startCommand)
+    (Filters.regex(re.compile(r'boa noite', re.IGNORECASE)))), startCommand )
 # re.compile e re.ignorecase é para ignorar o case sensitive para letras maiusculas e minusculas
 boleto_handler=MessageHandler(Filters.regex(re.compile(r'boleto', re.IGNORECASE)), boleto)
-contato_handler=MessageHandler(Filters.regex(re.compile(r'contato', re.IGNORECASE)), contato)
+contato_handler=MessageHandler((Filters.regex(re.compile(r'contato', re.IGNORECASE)) | Filters.regex(re.compile(r'contatos', re.IGNORECASE)) ) , contato)
 incentivo_handler=MessageHandler(Filters.regex(re.compile(r'incentivo', re.IGNORECASE)), incentivo)
 nucleo_handler=MessageHandler(Filters.regex(re.compile(r'nucleo', re.IGNORECASE)), nucleo)
 convenio_handler=MessageHandler(Filters.regex(re.compile(r'convenio', re.IGNORECASE)), convenio)
@@ -127,9 +122,9 @@ pesquisa_handler=MessageHandler(Filters.regex(re.compile(r'pesquisa', re.IGNOREC
 etica_handler=MessageHandler((Filters.regex(re.compile(r'etica', re.IGNORECASE)) | (Filters.regex(re.compile(r'ética', re.IGNORECASE)))), etica)
 secretaria_handler=MessageHandler(Filters.regex(re.compile(r'secretaria', re.IGNORECASE)), secretaria)
 vestibular_handler=MessageHandler(Filters.regex(re.compile(r'vestibular', re.IGNORECASE)), vestibular)
-
-# para comandas ou palavras nao reconhecidas tem que ficar no final
-unknown_handler=MessageHandler(Filters.command, unknown)
+portal_handler=MessageHandler(Filters.regex(re.compile(r'portal', re.IGNORECASE)), portalAluno)
+editais_handler=MessageHandler((Filters.regex(re.compile(r'editais', re.IGNORECASE)) | (Filters.regex(re.compile(r'edital', re.IGNORECASE)))), editais)
+filtro_handler=MessageHandler((Filters.regex(re.compile(r'filtro', re.IGNORECASE))), filtro)
 
 # Here we add the handlers to the dispatcher
 dispatcher.add_handler(start_command_handler)
@@ -142,11 +137,11 @@ dispatcher.add_handler(gp_handler)
 dispatcher.add_handler(pesquisa_handler)
 dispatcher.add_handler(etica_handler)
 dispatcher.add_handler(secretaria_handler)
+dispatcher.add_handler(portal_handler)
 dispatcher.add_handler(vestibular_handler)
-dispatcher.add_handler(unknown_handler)
-
+dispatcher.add_handler(editais_handler)
+dispatcher.add_handler(filtro_handler)
 # Start search for updates
 updater.start_polling(clean=True)
-
 # Stop the bot, if Ctrl + C were pressed
 updater.idle()
